558b02b7a74dbcb38734427b41b562b9e5fdcb84
Sreeprakash Neelkantan <sree@schogini.com>
2024-12-07 21:26:53 +0530

    0001/demo-1

--- 0001/demo-1/app.py
+++ 0001/demo-1/app.py
@@ -5,7 +5,7 @@ import requests
 # Load environment variables from .env file
 load_dotenv()
 
-def generate_chat_response(conversation_history, api_key):
+def generate_chat_response_b4(conversation_history, api_key):
     """Generate a chat response using the OpenAI API."""
     api_url = "https://api.openai.com/v1/chat/completions"
     headers = {
@@ -28,6 +28,21 @@ def generate_chat_response(conversation_history, api_key):
         print(f"Error in chat response: {response.status_code}, {response.text}")
         return None
 
+def generate_chat_response(conversation_history, api_key):
+    """Generate a chat response using the OpenAI API."""
+	from openai import OpenAI
+	client = OpenAI()
+
+	completion = client.chat.completions.create(
+	  model="gpt-4o-mini",
+	  messages = conversation_history,
+	  # messages=[
+	  #   {"role": "system", "content": "You are a helpful assistant."},
+	  #   {"role": "user", "content": "Hello!"}
+	  # ]
+	)
+
+	return completion.choices[0].message
 
 if __name__ == "__main__":
     # Configuration
